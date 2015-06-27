19613446a23c1ac9c9ae67bb259af43350857430
Erik Hoversten <ehoversten@gmail.com>
2015-06-27 19:54:09 -0400

    Initial Commit

---
+++ NewToDoList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewToDoList
+//
+//  Created by Erik Hoversten on 27.06.15.
+//  Copyright (c) 2015 EverProductions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NewToDoListTests/NewToDoListTests.m
@@ -0,0 +1,40 @@
+//
+//  NewToDoListTests.m
+//  NewToDoListTests
+//
+//  Created by Erik Hoversten on 27.06.15.
+//  Copyright (c) 2015 EverProductions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface NewToDoListTests : XCTestCase
+
+@end
+
+@implementation NewToDoListTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
